8fb8d0b7c996f37c6e1ebf66ad216d804e4ef9d6
Rushing <mingli@ishanshan.com>
2019-01-09 10:08:50 +0800

    update:20190109-03

---
+++ ss-api-gateway/src/main/java/com/ishanshan/ssapigateway/SsApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.ishanshan.ssapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsApiGatewayApplication.class, args);
+	}
+
+}
+
